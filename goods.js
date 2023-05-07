ba8ff706f4ccae0e7c03be8ce9b45f0b52158bde
Serёga Lisenkov <99216176+SeregaIgorevichLisenkov@users.noreply.github.com>
2023-05-07 11:42:51 +0300

    я сделяль

---
+++ goods.js
@@ -0,0 +1,31 @@
+const GOODS = [{
+	id: 1,
+    name: 'Шлёпа',
+	price: 111,
+	imgSrc: 'goods/shlepa.png',
+}, {
+	id: 2,
+	name: 'Капибара',
+	price: 222,
+	imgSrc: 'goods/kapibara.png',
+}, {
+	id: 3,
+	name: 'Тяги',
+	price: 333,
+	imgSrc: 'goods/tyagi.png',
+}, {
+	id: 4,
+	name: 'Блинный Кот',
+	price: 444,
+	imgSrc: 'goods/blin.png',
+}, {
+	id: 5,
+	name: 'Смекалочка',
+	price: 555,
+	imgSrc: 'goods/smekalochka.png',
+}, {
+	id: 6,
+	//name: 'Доктор Ливси',
+	//price: 666,
+	imgSrc: 'goods/doctor livesey.png',
+}];
