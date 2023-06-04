3be580c496857df5baa8d87d735c73effc3c2b97
Serёga Lisenkov <99216176+SeregaIgorevichLisenkov@users.noreply.github.com>
2023-06-04 03:46:14 +0300

    счетчик + куки

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let activeTabId = 'cart';
 let goodsInCart = [];
+let totalPrice = 0; // : добавляем переменную суммарной стоимости
 
 const initialTab = getActiveTab();
 
@@ -32,84 +33,97 @@ function clickHandler(event) {
 	renderTabContentById(activeTabId);
 }
 
-function addInCartHandler(product) {
-	return () => {
-		let hasProduct = false;
-		let index = null;
-		let count = 1;
+// ---
 
-		for (let i = 0; i < goodsInCart.length; i++) {
-			const productInCart = goodsInCart[i];
+function addInCartHandler(product) { return () => { let hasProduct = false; let index = null; let count = 1;
 
-			if (product.id === productInCart.id) {
-				hasProduct = true;
-				index = i;
-				count = productInCart.count;
-			}
+	for (let i = 0; i < goodsInCart.length; i++) {
+		const productInCart = goodsInCart[i];
+
+		if (product.id === productInCart.id) {
+			hasProduct = true;
+			index = i;
+			count = productInCart.count;
 		}
+	}
 
-		if (hasProduct) {
-			goodsInCart[index].count = count + 1;
+	if (hasProduct) {
+		goodsInCart[index].count = count + 1;
+	}
+	else {
+		const productWithCount = product;
+		productWithCount.count = count;
+
+		goodsInCart.push(productWithCount);
+	}
+
+	// ---
+	// : подсчет общей стоимости товаров в корзине
+	totalPrice += product.price;
+
+	let fullSize = 0;
+
+	for (let i = 0; i < goodsInCart.length; i++) {
+		const productInCart = goodsInCart[i];
+		fullSize += productInCart.count;
+	}
+
+	tabWithCounter.dataset.goodsCount = fullSize;
+
+	// : добавляем обновление общей стоимости на странице
+	const totalPriceElem = document.querySelector('.total-price');
+	totalPriceElem.textContent = `Сумма: ₽ ${totalPrice}`;
+};
+}
+
+// ---
+
+function removeInCartHandler(productId) { return () => { const newGoodsInCart = [];
+
+	for (let i = 0; i < goodsInCart.length; i++) {
+		const product = goodsInCart[i];
+
+		if (productId === product.id) {
+			// : вычитаем стоимость удаленного товара из общей суммы
+			totalPrice -= product.price;
+
+			if (product.count > 1) {
+				newGoodsInCart.push({
+					id: product.id,
+					name: product.name,
+					price: product.price,
+					imgSrc: product.imgSrc,
+					count: product.count - 1,
+				});
+			}
+
+			updateCartItem(product.id, product.count);
 		}
 		else {
-			const productWithCount = product;
-			productWithCount.count = count;
-
-			goodsInCart.push(productWithCount);
+			newGoodsInCart.push(product);
 		}
+	}
 
-		// ---
+	goodsInCart = newGoodsInCart;
 
-		let fullSize = 0;
+	// ---
 
-		for (let i = 0; i < goodsInCart.length; i++) {
-			const productInCart = goodsInCart[i];
-			fullSize += productInCart.count;
-		}
+	let fullSize = 0;
 
-		tabWithCounter.dataset.goodsCount = fullSize;
-	};
+	for (let i = 0; i < goodsInCart.length; i++) {
+		const productInCart = goodsInCart[i];
+		fullSize += productInCart.count;
+	}
+
+	tabWithCounter.dataset.goodsCount = fullSize;
+
+	// : добавляем обновление общей стоимости на странице
+	const totalPriceElem = document.querySelector('.total-price');
+	totalPriceElem.textContent = `Сумма: ₽ ${totalPrice}`;
+};
 }
 
-function removeInCartHandler(productId) {
-	return () => {
-		const newGoodsInCart = [];
-
-		for (let i = 0; i < goodsInCart.length; i++) {
-			const product = goodsInCart[i];
-
-			if (productId === product.id) {
-				if (product.count > 1) {
-					newGoodsInCart.push({
-						id: product.id,
-						name: product.name,
-						price: product.price,
-						imgSrc: product.imgSrc,
-						count: product.count - 1,
-					});
-				}
-
-				updateCartItem(product.id, product.count);
-			}
-			else {
-				newGoodsInCart.push(product);
-			}
-		}
-
-		goodsInCart = newGoodsInCart;
-
-		// ---
-
-		let fullSize = 0;
-
-		for (let i = 0; i < goodsInCart.length; i++) {
-			const productInCart = goodsInCart[i];
-			fullSize += productInCart.count;
-		}
-
-		tabWithCounter.dataset.goodsCount = fullSize;
-	};
-}
+// ---
 
 function addClickListeners(elements, callback) {
 	for (let i = 0; i < elements.length; i++) {
@@ -241,38 +255,150 @@ function renderGoods_2() { // тут меняем (метод рендера)
 	return div;
 }
 
-function renderCart() {
-	const container = document.createElement('div');
-	container.dataset.activeTabContent = 'true';
-	container.className = 'cart-items';
+function renderCart() { const container = document.createElement('div'); 
+container.dataset.activeTabContent = 'true'; container.className = 'cart-items';
+
+for (let i = 0; i < goodsInCart.length; i++) {
+	const product = goodsInCart[i];
+
+	const cartItem = document.createElement('div');
+	cartItem.dataset.elementId = product.id;
+	cartItem.className = 'cart-item';
+
+	// : добавляем кнопки "-" и "+"
+	const minusButton = document.createElement('button');
+	minusButton.className = 'cart-item-change cart-item-minus';
+	minusButton.innerHTML = '-';
+	minusButton.addEventListener('click', decrementCountHandler(product.id));
+
+	const plusButton = document.createElement('button');
+	plusButton.className = 'cart-item-change cart-item-plus';
+	plusButton.innerHTML = '+';
+	plusButton.addEventListener('click', incrementCountHandler(product.id));
+
+	const countSpan = document.createElement('span');
+	countSpan.className = 'cart-item-count';
+	countSpan.textContent = product.count;
+
+	const countContainer = document.createElement('div');
+	countContainer.className = 'cart-item-count-container';
+	countContainer.append(minusButton, countSpan, plusButton);
+
+	cartItem.innerHTML = `
+<div class="cart-item-img"><img src="${product.imgSrc}"></div>
+<div class="cart-item-title">${product.name}</div>
+<div class="cart-item-price">₽ ${product.price}</div>
+`;
+
+	cartItem.append(countContainer);
+
+	const clickHander = removeInCartHandler(product.id);
+
+	const button = document.createElement('button');
+	button.className = 'cart-item-delete';
+	//button.textContent = 'x';
+	button.addEventListener('click', clickHander);
+
+	cartItem.append(button);
+
+	container.append(cartItem);
+}
+
+// : добавляем отображение общей стоимости на странице
+const totalPriceElem = document.createElement('div');
+totalPriceElem.className = 'total-price';
+totalPriceElem.textContent = `Сумма: ₽ ${totalPrice}`;
+
+container.append(totalPriceElem);
+
+return container;
+}
+
+// ---
+
+function decrementCountHandler(productId) { return () => { const newGoodsInCart = [];
 
 	for (let i = 0; i < goodsInCart.length; i++) {
 		const product = goodsInCart[i];
 
-		const cartItem = document.createElement('div');
-		cartItem.dataset.elementId = product.id;
-		cartItem.className = 'cart-item';
-		cartItem.innerHTML = `
-	<div class="cart-item-img"><img src="${product.imgSrc}"></div>
-	<div class="cart-item-title">${product.name}</div>
-	<div class="cart-item-count">${product.count} шт.</div>
-	<div class="cart-item-price">₽ ${product.price}</div>
-		`;
+		if (productId === product.id) {
+			// : вычитаем стоимость удаленного товара из общей суммы
+			totalPrice -= product.price;
 
-		const clickHander = removeInCartHandler(product.id);
+			if (product.count > 1) {
+				newGoodsInCart.push({
+					id: product.id,
+					name: product.name,
+					price: product.price,
+					imgSrc: product.imgSrc,
+					count: product.count - 1,
+				});
 
-		const button = document.createElement('button');
-		button.className = 'cart-item-delete';
-		button.textContent = 'x';
-		button.addEventListener('click', clickHander);
-
-		cartItem.append(button);
-
-		container.append(cartItem);
+				updateCartItem(product.id, product.count - 1);
+			} else {
+				updateCartItem(product.id, 0);
+			}
+		}
+		else {
+			newGoodsInCart.push(product);
+		}
 	}
 
-	return container;
+	goodsInCart = newGoodsInCart;
+
+	let fullSize = 0;
+
+	for (let i = 0; i < goodsInCart.length; i++) {
+		const productInCart = goodsInCart[i];
+		fullSize += productInCart.count;
+	}
+
+	tabWithCounter.dataset.goodsCount = fullSize;
+};
 }
+
+function incrementCountHandler(productId) { return () => { const newGoodsInCart = [];
+
+	for (let i = 0; i < goodsInCart.length; i++) {
+		const product = goodsInCart[i];
+
+		if (productId === product.id) {
+			newGoodsInCart.push({
+				id: product.id,
+				name: product.name,
+				price: product.price,
+				imgSrc: product.imgSrc,
+				count: product.count + 1,
+			});
+
+			updateCartItem(product.id, product.count + 1);
+
+			// : добавляем стоимость добавленного товара в общую сумму
+			totalPrice += product.price;
+		}
+		else {
+			newGoodsInCart.push(product);
+		}
+	}
+
+	goodsInCart = newGoodsInCart;
+
+	let fullSize = 0;
+
+	for (let i = 0; i < goodsInCart.length; i++) {
+		const productInCart = goodsInCart[i];
+		fullSize += productInCart.count;
+	}
+
+	tabWithCounter.dataset.goodsCount = fullSize;
+
+	// : добавляем обновление общей стоимости на странице
+	const totalPriceElem = document.querySelector('.total-price');
+	totalPriceElem.textContent = `Сумма: ₽ ${totalPrice}`;
+};
+}
+
+// ---
 
 function updateCartItem(id, count) {
 	const cartItem = document.querySelector(`[data-element-id="${id}"]`);
@@ -285,6 +411,8 @@ function updateCartItem(id, count) {
 		cartItem.remove();
 	}
 }
+
+
 // ТЁМНАЯ ТЕМА
 // выбираем кнопку
 const btn = document.querySelector(".btn-toggle");
@@ -295,13 +423,179 @@ btn.addEventListener("click", function () {
 });
 
 
+// ПЕЧЕНЬКИ
+// Функция для получения содержимого корзины из куки
+function getGoodsInCartFromCookie() {
+	// Получаем сериализованные данные из куки по ключу 'cartGoods'
+	const serializedData = getCookie('cartGoods');
+	if (!serializedData) {
+		// Если данных в куки нет, возвращаем пустой массив
+		return [];
+	}
+	try {
+		// Парсим сериализованные данные и возвращаем их в виде массива объектов
+		return JSON.parse(serializedData);
+	} catch {
+		// Если произошла ошибка при парсинге, очищаем куки и возвращаем пустой массив
+		setCookie('cartGoods', '');
+		return [];
+	}
+}
 
+// Функция для сохранения содержимого корзины в куки
+function updateGoodsInCartCookie() {
+	// Сохраняем сериализованные данные корзины в куки по ключу 'cartGoods'
+	setCookie('cartGoods', goodsInCart);
+}
 
+// Функция для установки значения куки
+function setCookie(name, value, options={}) {
+	// Устанавливаем опции по умолчанию и объединяем их с переданными опциями
+	options = {
+		path: '/',
+		...options
+	};
 
+	// Сохраняем опцию 'expires' в отдельной переменной для удобства
+	let expires = options.expires;
 
+	// Если опция 'expires' передана в виде числа, преобразуем ее в объект Date
+	if (expires && typeof expires === 'number') {
+		const date = new Date();
+		date.setTime(date.getTime() + expires * 1000);
+		expires = options.expires = date;
+	}
 
+	// Если опция 'expires' передана в виде объекта Date, преобразуем ее в строку формата UTC
+	if (expires && expires.toUTCString) {
+		options.expires = expires.toUTCString();
+	}
 
+	// Сериализуем значение куки и добавляем его в строку куки
+	value = encodeURIComponent(JSON.stringify(value));
 
+	let updatedCookie = `${name}=${value}`;
 
+	for (let optionKey in options) {
+		updatedCookie += `; ${optionKey}`;
+		let optionValue = options[optionKey];
+		if (optionValue !== true) {
+			updatedCookie += `=${optionValue}`;
+		}
+	}
 
+	// Устанавливаем новое значение куки
+	document.cookie = updatedCookie;
+}
 
+// Функция для получения значения куки по ключу
+function getCookie(name) {
+	// Ищем по регулярному выражению значение куки с указанным ключом и декодируем его
+	const matches = document.cookie.match(new RegExp(
+		`(?:^|; )${name.replace(/([.$?*|{}()[]\\\/\+^])/g, '\\$1')}=([^;]*)`
+	));
+	return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+// Обработчик события добавления товара в корзину
+function addToCartHandler(productId) {
+	return () => {
+		const product = getProductById(productId);
+
+		// Создаем копию массива корзины
+		const newGoodsInCart = [...goodsInCart];
+
+		let isInCart = false;
+
+		for (let i = 0; i < newGoodsInCart.length; i++) {
+			const productInCart = newGoodsInCart[i];
+
+			// Если товар уже есть в корзине, увеличиваем количество
+			if (productId === productInCart.id) {
+				isInCart = true;
+
+				newGoodsInCart[i] = {
+					id: productInCart.id,
+					name: productInCart.name,
+					price: productInCart.price,
+					imgSrc: productInCart.imgSrc,
+					count: productInCart.count + 1,
+				};
+
+				// Обновляем количество товара в соответствующей строке таблицы
+				updateCartItem(productId);
+				break;
+			}
+		}
+
+		// Если товара еще нет в корзине, добавляем его
+		if (!isInCart) {
+			newGoodsInCart.push({
+				id: product.id,
+				name: product.name,
+				price: product.price,
+				imgSrc: product.imgSrc,
+				count: 1,
+			});
+
+			// Создаем новую строку таблицы с информацией о товаре
+			createCartItem(product.id);
+		}
+
+		// Обновляем данные корзины
+		goodsInCart = newGoodsInCart;
+
+		let fullSize = 0;
+
+		for (let i = 0; i < newGoodsInCart.length; i++) {
+			const productInCart = newGoodsInCart[i];
+			fullSize += productInCart.count;
+		}
+
+		tabWithCounter.dataset.goodsCount = fullSize;
+
+		// Сохраняем содержимое корзины в куки
+		updateGoodsInCartCookie();
+	};
+}
+
+// Обработчик события удаления товара из корзины
+function removeInCartHandler(productId) {
+	return () => {
+		const newGoodsInCart = [];
+
+		for (let i = 0; i < goodsInCart.length; i++) {
+			const product = goodsInCart[i];
+
+			if (productId === product.id) {
+				// Вычитаем стоимость удаленного товара из общей суммы
+				totalPrice -= product.price * product.count;
+
+				// Обновляем количество товара в соответствующей строке таблицы на 0
+				updateCartItem(product.id, 0);
+			}
+			else {
+				newGoodsInCart.push(product);
+			}
+		}
+
+		// Обновляем данные корзины
+		goodsInCart = newGoodsInCart;
+
+		let fullSize = 0;
+
+		for (let i = 0; i < goodsInCart.length; i++) {
+			const productInCart = goodsInCart[i];
+			fullSize += productInCart.count;
+		}
+
+		tabWithCounter.dataset.goodsCount = fullSize;
+
+		// Сохраняем содержимое корзины в куки
+		updateGoodsInCartCookie();
+
+		// Обновляем общую стоимость на странице
+		const totalPriceElem = document.querySelector('.total-price');
+		totalPriceElem.textContent = `Сумма: ₽ ${totalPrice}`;
+	};
+}
